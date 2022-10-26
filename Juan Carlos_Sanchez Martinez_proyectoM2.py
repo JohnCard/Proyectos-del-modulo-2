38c56f217bf07ec0edb1c0f80dcfb2bdb2f5a2bd
JohnCard <107288337+JohnCard@users.noreply.github.com>
2022-10-26 14:37:43 -0500

    2:37 p. m.

--- Juan Carlos_Sanchez Martinez_proyectoM2.py
+++ Juan Carlos_Sanchez Martinez_proyectoM2.py
@@ -6,14 +6,21 @@ variable_numeros = '-123456789.'
 
 # Esta funcion ayudará a verificar que el usuario no digite mal un dato de tipo numerico o float al momento de digitar coordenadas para
 # estudiar sus cuadrantes en el que se encuentren
-def evaluar_variable_uno(param):
+def evaluar_variable_uno(param,variable):
     cont = 0
+    if(param.count('.') > 1 or param.count('-') > 1 or param == '0'):
+        cont += 1
     for i in param:
         # el objetivo de esta condicional esque si se cumple almenos una vez la condición, se le aumentará un uno, y para esta 
         # variable (cont_tres) solo basta con un uno para indicar que hay un error en el dato del usuario
-        if(i not in variable_numeros or param.count('.') > 1 or param.count('-') > 1 or param == '0'):
+        if(i not in variable):
             cont += 1
-    return cont
+    return int(cont)
+
+def evaluar_variable_dos(cadena):
+    while(evaluar_variable_uno(cadena,variable_numeros)>0):
+        cadena = input('Favor de intentarlo de nuevo: ')
+    return float(cadena)
 
 # Estas variables se usaran a lo largo de este programa para que al preguntarle al usuario de si le gusto o no el programa, estas 
 # cuenten de uno en uno las cantidad de valoraciones ya sean positivas o negativas dependiendo de si el usuario responde con un "Y" o un
@@ -24,17 +31,16 @@ cont = 0; cont_seg = 0
 # rá a responder algo diferente de los incisos a) ó b), le lanzará al usuario que debe digitar su respuesta de nuevo.
 def leervar_curt(param,letra_uno,letra_dos,letra_tres,letra_cuatro):
     while(param != letra_uno and param != letra_dos and param != letra_tres and param != letra_cuatro):
-        param = input('Favor de ingresar de nuevo el dato por favor (A/B): ')
+        param = input(f'Favor de ingresar de nuevo el dato por favor ({letra_uno}/{letra_tres}): ')
     return param
     
 # Esta es la función dedicada a leer coordenadas de tipo 2D en caso de que el usuario digite la "a" para el inciso a) 
 def CalC_Cuad():
     x = input('Favor de digitar la coordenada en "X": ')
     # Usamos la funcion evld_var() para validar el dato de manera correcta
-    x = evld_var(x) 
-    
+    x = evaluar_variable_dos(x) 
     y = input('Favor de digitar la coordenada en "Y": ')
-    y = evld_var(y)
+    y = evaluar_variable_dos(y)
     
     # Y de a cuerdo a las condiciones en las que se encuentre la coordenada, verificará a que cuadrante peertenece
     if(x > 0 and y > 0):
@@ -50,14 +56,14 @@ def CalC_Cuad():
 def CalC_Cuad_seg():
     x = input('Favor de digitar la coordenada en "X": ')
     # Aplicamos las mismas logicas que con la CalC_Cuad()
-    x = evld_var(x)
+    x = evaluar_variable_dos(x)
     
     y = input('Favor de digitar la coordenada en "Y": ')
-    y = evld_var(y)
+    y = evaluar_variable_dos(y)
     
     z = input('Favor de digitar la coordenada en "Z": ')
     # Solo que esta vez agregandole la coordenada 3ra en z
-    z = evld_var(z)
+    z = evaluar_variable_dos(z)
     
     # Y en esta ocasión evaluará pero para un octante
     if(x > 0 and y > 0 and z > 0):
@@ -78,32 +84,31 @@ def CalC_Cuad_seg():
         print('Su punto se encuentra en el octante VII')
 
     
-answer = 'y'
-while(answer_prim == 'y' or answer_prim == 'Y'):
+answer = 's'
+while(answer == 's' or answer == 'S'):
     print("""En que plano desea trabajar? 
-a) Plano 2D
-b) Plano 3D""")
+a) Plano 2D,\tb) Plano 3D""")
     # preguntamos por el plano (a o b) en el que desea trabajar
-    answer_seg = input('Digite su respuesta (a/b): ')
+    answer = input('Digite su respuesta (a/b): ')
     # nos aseguramos de que sea correcta entre "a" ó "b"
-    answer_seg = leervar_curt(answer_seg)
+    answer = leervar_curt(answer,'A','a','B','b')
     # Y nos enfocamos en llamar a la función correcta de a cuerdo al inciso que haya digitado
-    if(answer_seg == 'a' or answer_seg == 'A'):
+    if(answer == 'a' or answer == 'A'):
         CalC_Cuad()
-    elif(answer_seg == 'b' or answer_seg == 'B'):
+    elif(answer == 'b' or answer == 'B'):
         CalC_Cuad_seg()
     # Valoramos la experiencia que haya tenido nuestro usuario
-    answer_tir = input('Le gusto nuestro servicio (Y/N)? ')
-    answer_tir = leervar_quint(answer_tir)
+    answer = input('Le gusto nuestro servicio (S/N)? ')
+    answer = leervar_curt(answer,'S','s','N','n')
     
     # Luego de asegurarnos de que haya respondido bien entre "y" o "n" con leervar_quint(), dependiendo de la respuesta,
     # le sumamos de uno en uno a alguna de nuestras variables anteriormente explicadas
-    if(answer_tir == 'y' or answer_tir == 'Y'):
+    if(answer == 's' or answer == 'S'):
         cont += 1
-    elif(answer_tir == 'n' or answer_tir == 'N'):
+    elif(answer == 'n' or answer == 'N'):
         cont_seg += 1
-    answer_prim = input('Desea volver a intentarlo (Y/N)? ')
-    answer_prim = leervar_quint(answer_prim)
+    answer = input('Desea volver a intentarlo (S/N)? ')
+    answer = leervar_curt(answer,'S','s','N','n')
 if(cont > cont_seg):
     conclusión = 'A la gente le gusto mas el programa de lo que no le gusto'
 elif(cont_seg > cont):
@@ -133,7 +138,6 @@ while(answer == 'S' or answer == 's'):
         print(f'Sobran letras. Tiene "{cantidad}" letras.')
     
     answer = input('Le gustaria volver a intentarlo (S/N)? ')
-    while(answer != 'S' and answer != 's' and answer != 'n' and answer != 'N'):
-        answer = input(f'El dato {answer} es incorrecto, favor de digitarlo de nuevo: ')
+    answer = leervar_curt(answer,'S','s','N','n')
 
 print('Usted ya ha llegado al final del programa, gracias por su visita.')
